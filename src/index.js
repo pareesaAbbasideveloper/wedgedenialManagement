3e653d8c64934fd39bb456e03853bad1626185b1
Pareesa Abbasi <pareesa.developer@gmail.com>
2026-03-07 06:13:19 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { startWorker } = require("./worker/aiWorker");
 
 async function main() {
-  const insuranceNumber = "+923193023322";
+  const insuranceNumber = "+19544595315";
   const roomName = "call-room-2";
   await startWorker(roomName);
   // await callInsurance(insuranceNumber, roomName);
