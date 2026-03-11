cacf5f1783b5dad30733c0fba195f9457971c1ef
Pareesa Abbasi <pareesa.developer@gmail.com>
2026-03-12 03:37:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ async function main() {
   const insuranceNumber = "+19544595315";
   const roomName = "call-room-2";
   await startWorker(roomName);
-  // await callInsurance(insuranceNumber, roomName);
+  await callInsurance(insuranceNumber, roomName);
 }
 
 main();
